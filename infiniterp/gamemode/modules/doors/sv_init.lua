1073db1619e5884191621f9d51e77db60e7cbaaf
Kohltastrophe <kohltastrophe@yahoo.com>
2014-03-29 19:11:10 -0500

    door

---
+++ infiniterp/gamemode/modules/doors/sv_init.lua
@@ -0,0 +1,12 @@
+function DoorValues(ent)
+	if not IsValid(ent) then print("[Error] ("..ent..") Is not valid :c") return false end
+	local class = ent:GetClass()
+	if class == "func_door" or class == "func_door_rotating" or class == "prop_door_rotating" or class == "prop_dynamic" then
+		if not class == "prop_dynamic" then ent.IsDoorOwnable = true end
+		if not IsValid(ent.DoorOwners) then ent.DoorOwners = {} end
+		ent.IsDoor = true
+		ent.DoorUnlocked = false
+		ent.DoorTitle = ""
+	elseif class == "prop_dynamic" then door = ent.IsDoor = true end
+	end
+end
