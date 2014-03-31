784725f5dd321182f2f3b73f79122d16ec709f1e
Darkaaja <darkaaja@hotmail.com>
2014-03-31 15:33:36 -0700

    Added Client Funcs + Added Functions to Sever

--- infiniterp/gamemode/modules/functions/cl_generalfunctions.lua
+++ infiniterp/gamemode/modules/functions/cl_generalfunctions.lua
@@ -3,21 +3,8 @@ Below is a simple index for functions.
 All functions work across the game-mode.
 The Table of Contents should have a category line such as ///[001]: General/// before the functions to make it easier to edit.
 All functions should be prefixed with inf_ to make it gamemode specific.
-this is server sided only.
+This is client sided only
+
 [Table of Contents]
 [001]: General
 */
-
-
-
-///[001]: General///
-
-function GM:PlayerConnect(plr)
-	if not plr:GetPData("money", nil) then
-		plr:SetPData("money", 1000)
-	end
-end
-
-function ENT:getMoney()
-	return plr:GetPData("money", 0) or 'Nil'
-end
