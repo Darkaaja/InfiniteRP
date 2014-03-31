25378d2a3eea71325784da9941b08b78cef03a02
Darkaaja <darkaaja@hotmail.com>
2014-03-31 13:13:18 -0700

    Added Generic Functions Script

---
+++ infiniterp/gamemode/modules/functions/sv_generalfunctions.lua
@@ -0,0 +1,16 @@
+/*
+Below is a simple index for functions.
+All functions work across the game-mode.
+The Table of Contents should have a prefixed line such as ///[001]: Money/// to make it easier to edit.
+All functions should be prefixed with inf_ to make it gamemode specific.
+
+[Table of Contents]
+[001]: Money
+*/
+
+
+
+///[001]: Money///
+function inf_dropMoney()
+	return false
+end
